a6ad8c96bf6ba8601604bb6a9eca2bafc54c1044
Nikolai <nikolai.kuschaev@gmail.com>
2019-05-08 21:39:26 +0300

    Message

--- src/Kinoafisha/index.js
+++ src/Kinoafisha/index.js
@@ -1,16 +1,18 @@
 // Core
 import React, { useState, useEffect } from 'react';
+import cx from 'classnames';
 
 // Instruments
 import './styles/main.css';
 import { getStyles } from '../instruments';
 
 // Api
-import { api } from '../Api';
+import { api } from '../API';
 
 export const Kinoafisha = () => {
     const [ selectedFilter, setSelectedFilter ] = useState('upcoming');
     const [ movies, setMovies ] = useState([]);
+    const [ selectedMovie, setSelectedMovie ] = useState('');
 
     const styles = getStyles({
         selectedFilter,
@@ -24,9 +26,28 @@ export const Kinoafisha = () => {
         };
 
         getMoviesByFilter();
-    }, []);
+    }, [ selectedFilter ]);
 
-    console.log(movies);
+    const moviesJSX = movies.map((movie) => {
+        const posterStyle = cx('poster', {
+            selectedPoster: movie.id === selectedMovie,
+        });
+
+        return (
+            <div
+                className = 'movie'
+                key = { movie.id }>
+                <div
+                    className = { posterStyle }
+                    onClick = { () => setSelectedMovie(movie.id) }>
+                    <span className = 'genre'>{movie.genre}</span>
+                    <img src = { movie.poster } />
+                    <span className = 'rating'>{movie.rating}</span>
+                </div>
+                <span className = 'title'>{movie.title}</span>
+            </div>
+        );
+    });
 
     return (
         <>
@@ -36,26 +57,24 @@ export const Kinoafisha = () => {
                     <div
                         className = { styles.latestFilter }
                         data-name = 'latest'
-                        onClick = { (event) => setSelectedFilter(event.currentTarget.dataset.name)
-                        }>
+                        onClick = { (event) => setSelectedFilter(event.currentTarget.dataset.name) }>
                         <span>Новинки 2019</span>
                     </div>
                     <div
                         className = { styles.upcomingFilter }
                         data-name = 'upcoming'
-                        onClick = { (event) => setSelectedFilter(event.currentTarget.dataset.name)
-                        }>
+                        onClick = { (event) => setSelectedFilter(event.currentTarget.dataset.name) }>
                         <span>Скоро в кинотеатрах</span>
                     </div>
                     <div
                         className = { styles.popularFilter }
                         data-name = 'popular'
-                        onClick = { (event) => setSelectedFilter(event.currentTarget.dataset.name)
-                        }>
+                        onClick = { (event) => setSelectedFilter(event.currentTarget.dataset.name) }>
                         <span>В топ-чартах</span>
                     </div>
                 </div>
             </div>
+            <div className = 'content'>{moviesJSX}</div>
         </>
     );
 };
